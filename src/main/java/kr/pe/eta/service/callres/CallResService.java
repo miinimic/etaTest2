4c47b3eacffcd67371e20602e4eb79a97f7ab14c
Hyunggoo Kim <chunhab123@gmail.com>
2023-11-28 10:16:27 +0900

    Fix: callRes 수정
    pom.xml, application.properties, impl, dao, service, mapper

--- src/main/java/kr/pe/eta/service/callres/CallResService.java
+++ src/main/java/kr/pe/eta/service/callres/CallResService.java
@@ -1,5 +1,26 @@
 package kr.pe.eta.service.callres;
 
+import java.util.Map;
+
+import kr.pe.eta.common.Search;
+import kr.pe.eta.domain.Call;
+import kr.pe.eta.domain.Pay;
+
 public interface CallResService {
+	public Call getRecord(int callNo) throws Exception;
+
+	public Call getReservation(int callNo) throws Exception;
+
+	public Map<String, Object> getRecordList(Search search, int userNo) throws Exception;
+
+	public Map<String, Object> getReservationList(Search search) throws Exception;
+
+	public void addPay(Pay pay) throws Exception;
+
+	public void updateCallStateCode(Call call) throws Exception;
+
+	public Map<String, Object> getCallResList(Search search) throws Exception;
+
+	public void updateEndXY(Call call) throws Exception;
 
 }
